b345ca9e984d3da28cb32f537a0fb02f660078d4
Grigory <ozeroffgrisha@gmail.com>
2024-02-09 19:38:16 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tink"
+include(":app")
+include(":presentation")
+include(":domain")
+include(":data")
